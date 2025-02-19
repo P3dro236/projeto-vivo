2f765a0f022f4209c05fb1006feb7ac5be957f09
Pedro <pedrohq238@gmail.com>
2025-02-19 01:36:07 -0300

    fix add and remove button

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,16 +1,26 @@
 $(document).ready(function () {
     const group = `<div class='mb-3 d-flex justify-content-center align-items-center group'>
             <select role='button' id='planos' class='form-select me-2'>
-                <option value=' selected disabled>Selecione o plano</option>
+                <option value='' selected disabled>Selecione o plano</option>
                 <option value='Basico'>Básico - R$50</option>
                 <option value='Intermediario'>Intermediário - R$80</option>
                 <option value='Avancado'>Avançado - R$120</option>
             </select>
-            <button id='addPlan' class='btn btn-success p-0'>+</button>
-            <button id='removePlan' class='btn btn-danger p-0'>-</button>
+            <button class='addPlan btn btn-success p-0'>+</button>
+            <button class='removePlan btn btn-danger p-0'>-</button>
         </div>`;
-        $(".container").on("click", ".addPlan", function() {
-            let newGroup = $(group);
-            $(".container").append(newGroup);
-        });
+
+    // Usar jQuery para garantir que os novos elementos sejam manipuláveis
+    $(".container").on("click", ".addPlan", function() {
+        let newGroup = $(group);  // Criação do novo grupo com jQuery
+        $(".container").append(newGroup);  // Adicionando o novo grupo à página
+    });
+
+    $(".container").on("click", ".removePlan", function() {
+        if ($(".group").length > 1) {
+            $(this).closest(".group").remove();  // Removendo o grupo clicado
+        } else{
+            alert("Você não pode remover o último plano!");
+        }
+    });
 });
