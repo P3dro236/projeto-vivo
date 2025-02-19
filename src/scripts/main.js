7e4a95cb875418a6f48fda6617ff0b8f937d10ee
Pedro <pedrohq238@gmail.com>
2025-02-19 01:56:31 -0300

    added the plans to an variable

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,26 +1,36 @@
 $(document).ready(function () {
+    const planos = [
+        { nome: 'Básico', valor: 'R$50' },
+        { nome: 'Intermediário', valor: 'R$80' },
+        { nome: 'Avançado', valor: 'R$120' },
+        { nome: 'Super', valor: 'R$200' }
+    ];
     const group = `<div class='mb-3 d-flex justify-content-center align-items-center group'>
-            <select role='button' id='planos' class='form-select me-2'>
+            <select class='form-select me-2'>
                 <option value='' selected disabled>Selecione o plano</option>
-                <option value='Basico'>Básico - R$50</option>
-                <option value='Intermediario'>Intermediário - R$80</option>
-                <option value='Avancado'>Avançado - R$120</option>
             </select>
             <button class='addPlan btn btn-success p-0'>+</button>
             <button class='removePlan btn btn-danger p-0'>-</button>
         </div>`;
+    function atualizarPlanos(select) {
+        $(select).empty();
+        $(select).append("<option value='' selected disabled>Selecione o plano</option>");
+        planos.forEach(function (plano) {
+            $(select).append(`<option value="${plano.nome}">${plano.nome} - ${plano.valor}</option>`);
+        });
+    }
 
-
+    atualizarPlanos($(".group select").first());
     $(".container").on("click", ".addPlan", function() {
-        let newGroup = $(group);
+        let newGroup = $(group); 
         $(this).closest(".group").after(newGroup)
+        atualizarPlanos(newGroup.find("select"));
     });
-
     $(".container").on("click", ".removePlan", function() {
         if ($(".group").length > 1) {
-            $(this).closest(".group").remove();
+            $(this).closest(".group").remove();  
         } else{
-            alert("Você não pode remover o último plano!");
+            alert("É necessário ter ao menos um plano!")
         }
     });
 });
