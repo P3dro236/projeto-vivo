f9f31a4054fd48f77b333d1888c4e77c3b0deb09
Pedro Henrique <pedrohq238@gmail.com>
2025-02-19 16:18:00 -0300

    initiate to insert buttons functions to a separete module

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,5 @@
+
+import { adicionarPlano, removerPlano } from 'buttons.js';
 $(document).ready(function () {
     const planos = [
         { nome: 'Controle 8GB', valor: 'R$XX,XX' },
@@ -5,6 +7,7 @@ $(document).ready(function () {
         { nome: 'Fámilia II', valor: 'R$XX,XX' },
         { nome: 'Fámilia V', valor: 'R$XX,XX' }
     ];
+    
     const group = `<div class='mb-3 d-flex justify-content-center align-items-center group'>
             <select class='form-select me-2' role='button'>
                 <option value='' selected disabled>Selecione o plano</option>
@@ -12,24 +15,22 @@ $(document).ready(function () {
             <button class='addPlan btn btn-success p-0'>+</button>
             <button class='removePlan btn btn-danger p-0'>-</button>
         </div>`;
-    function atualizarPlanos(select) {
+
+    function atualizarPlanos(select, planos) {
         $(select).empty();
         $(select).append("<option value='' selected disabled>Selecione o plano</option>");
         planos.forEach(function (plano) {
             $(select).append(`<option value="${plano.nome}">${plano.nome} - ${plano.valor}</option>`);
         });
     }
-    atualizarPlanos($(".group select").first());
+
+    atualizarPlanos($(".group select").first(), planos);
+
     $(".container").on("click", ".addPlan", function() {
-        let newGroup = $(group); 
-        $(this).closest(".group").after(newGroup)
-        atualizarPlanos(newGroup.find("select"));
+        adicionarPlano.call(this, group, planos);
     });
+
     $(".container").on("click", ".removePlan", function() {
-        if ($(".group").length > 1) {
-            $(this).closest(".group").remove();  
-        } else{
-            alert("É necessário ter ao menos um plano!")
-        }
+        removerPlano.call(this);
     });
 });
