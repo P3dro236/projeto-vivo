cd0c061c144bf4c33363fb61feda77aef54d488a
Pedro Henrique <pedrohq238@gmail.com>
2025-02-19 13:54:01 -0300

    fix the cursor on the added select and changed the plans to the actual placeholder plans

---
+++ index.js
@@ -0,0 +1,31 @@
+const { app, BrowserWindow  } = require('electron');
+const electronReload = require('electron-reload');
+const path = require('path');
+const fs = require('fs');
+
+let mainWindow;
+require('electron-reload')(__dirname, {
+    electron: path.join(__dirname, 'node_modules', '.bin', 'electron')
+});
+
+app.whenReady().then(() => {
+
+    mainWindow = new BrowserWindow({
+        width: 400,
+        height: 600,
+        resizable: false,
+        webPreferences: {
+            nodeIntegration: true
+        }
+    })
+    mainWindow.loadFile('src/index.html');
+
+    mainWindow.on('closed', () => {
+        mainWindow = null;
+    })
+})
+app.on("window-all-closed", () => {
+    if (process.platform !== 'darwin') {
+        app.quit();
+    }
+})
---
+++ src/scripts/main.js
@@ -0,0 +1,35 @@
+$(document).ready(function () {
+    const planos = [
+        { nome: 'Básico', valor: 'R$50' },
+        { nome: 'Intermediário', valor: 'R$80' },
+        { nome: 'Avançado', valor: 'R$120' },
+        { nome: 'Super', valor: 'R$200' }
+    ];
+    const group = `<div class='mb-3 d-flex justify-content-center align-items-center group'>
+            <select class='form-select me-2'>
+                <option value='' selected disabled>Selecione o plano</option>
+            </select>
+            <button class='addPlan btn btn-success p-0'>+</button>
+            <button class='removePlan btn btn-danger p-0'>-</button>
+        </div>`;
+    function atualizarPlanos(select) {
+        $(select).empty();
+        $(select).append("<option value='' selected disabled>Selecione o plano</option>");
+        planos.forEach(function (plano) {
+            $(select).append(`<option value="${plano.nome}">${plano.nome} - ${plano.valor}</option>`);
+        });
+    }
+    atualizarPlanos($(".group select").first());
+    $(".container").on("click", ".addPlan", function() {
+        let newGroup = $(group); 
+        $(this).closest(".group").after(newGroup)
+        atualizarPlanos(newGroup.find("select"));
+    });
+    $(".container").on("click", ".removePlan", function() {
+        if ($(".group").length > 1) {
+            $(this).closest(".group").remove();  
+        } else{
+            alert("É necessário ter ao menos um plano!")
+        }
+    });
+});
